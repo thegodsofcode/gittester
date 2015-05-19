688bbaf51157fba98ca13e600adc2f4016960f1b
epsooraj <epsooraj4@gmail.com>
2015-05-19 18:09:06 +0530

    s

--- app/src/main/java/gittest/bitverse/com/gittest/MainActivity.java
+++ app/src/main/java/gittest/bitverse/com/gittest/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    int b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
