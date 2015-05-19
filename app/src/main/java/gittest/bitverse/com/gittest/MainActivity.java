3526961d3561c126283d55164a448dae22a440d7
Christie <christeejacobs@gmail.com>
2015-05-19 18:09:26 +0530

    a

--- app/src/main/java/gittest/bitverse/com/gittest/MainActivity.java
+++ app/src/main/java/gittest/bitverse/com/gittest/MainActivity.java
@@ -28,6 +28,8 @@ public class MainActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+	int a;
+
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
